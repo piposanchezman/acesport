02e930148674472c82960a9c1ed83dc44308e042
Andrés Felipe Sánchez Manjarres <piposanchezman@gmail.com>
2024-03-26 12:35:26 -0500

    Feat: Add onboarding feature with slides and components

---
+++ components/Slides.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: 'Create your team',
+    description: 'Create a team and invite your friends to join',
+    image: require('../assets/onboarding1.png'),
+  },
+  {
+    id: 2,
+    title: 'Join a team',
+    description: 'Join a team by entering the team code',
+    image: require('../assets/onboarding2.webp'),
+  },
+  {
+    id: 3,
+    title: 'Play tournaments',
+    description: 'Play tournaments with your team and win prizes',
+    image: require('../assets/onboarding3.png'),
+  },
+];
