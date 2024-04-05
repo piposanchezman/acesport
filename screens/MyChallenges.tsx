c450df1927a0e607c6e82e6d5f0da79c5325e2f8
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2024-04-05 11:12:07 -0500

    Fix: Refactor MyChallenges component to include navigation to 'Nuevo Torneo'

--- screens/MyChallenges.tsx
+++ screens/MyChallenges.tsx
@@ -7,19 +7,32 @@ import {
   StyleSheet,
 } from 'react-native';
 
-const MyChallenges = () => {
+const MyChallenges = ({navigation}: {navigation: any}) => {
+  const handleCreateTournament = () => {
+    navigation.navigate('Nuevo Torneo');
+  };
+
   return (
     <ImageBackground
       source={require('../assets/Background.png')}
       style={{flex: 1, justifyContent: 'center'}}
       imageStyle={{opacity: 0.5}}>
-      <View style={{alignItems: 'center'}}>
+      <View
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#141414',
+          opacity: 0.75,
+          height: '100%',
+          justifyContent: 'center',
+        }}>
         <Text style={{fontSize: 32, color: '#f6ddb3', paddingTop: 80}}>
           NO HAY CHALLENGES
         </Text>
         <Text style={{fontSize: 24, color: '#f6ddb3'}}>Añade el primero</Text>
       </View>
-      <TouchableOpacity style={styles.addButton}>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={handleCreateTournament}>
         <Text style={styles.buttonText}>+</Text>
       </TouchableOpacity>
     </ImageBackground>
