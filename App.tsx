32fa790edf8793daf9d716c6f11c2a953149719b
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-21 16:29:21 -0500

    chore: Add ApiContextProvider to App.tsx for managing API context

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import "react-native-gesture-handler";
 import React from "react";
 import { Auth0Provider } from "react-native-auth0";
 import AppNav from "navigation/AppNav";
+import { ApiContextProvider } from "context/ApiContext";
 
 function App() {
   const auth0Domain = process.env.EXPO_PUBLIC_AUTH0_DOMAIN;
   const auth0ClientId = process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID;
   return (
     <Auth0Provider domain={auth0Domain as string} clientId={auth0ClientId as string}>
-      <AppNav />
+      <ApiContextProvider>
+        <AppNav />
+      </ApiContextProvider>
     </Auth0Provider>
   );
 }
