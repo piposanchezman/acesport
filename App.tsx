8a31f7e13362ad694b7a506b90c0b2ff6cc8bb8b
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2024-05-21 09:20:55 -0500

    chore: Update Auth0 domain and client ID in App.tsx

--- App.tsx
+++ App.tsx
@@ -4,11 +4,10 @@ import { Auth0Provider } from "react-native-auth0";
 import AppNav from "navigation/AppNav";
 
 function App() {
+  const auth0Domain = process.env.EXPO_PUBLIC_AUTH0_DOMAIN;
+  const auth0ClientId = process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID;
   return (
-    <Auth0Provider
-      domain={"dev-kllowhtqsd8qirzp.us.auth0.com"}
-      clientId={"vfNbwg8b9baJme8pmUHGWyNDCubVC3xE"}
-    >
+    <Auth0Provider domain={auth0Domain as string} clientId={auth0ClientId as string}>
       <AppNav />
     </Auth0Provider>
   );
